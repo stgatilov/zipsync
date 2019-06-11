064eb8b037765d68a451a1c5e526efbfcc607723
stgatilov <none@none>
2019-06-11 22:31:18 +0700

    Utils fixed...

--- Utils.cpp
+++ Utils.cpp
@@ -1,5 +1,17 @@
 #include "Utils.h"
+#include "tsassert.h"
 
 namespace TdmSync {
+
+StdioFileHolder::StdioFileHolder(FILE *f)
+    : StdioFileUniquePtr(f, fclose)
+{}
+StdioFileHolder::~StdioFileHolder()
+{}
+StdioFileHolder::StdioFileHolder(const char *path, const char *mode)
+    : StdioFileUniquePtr(fopen(path, mode), fclose)
+{
+    TdmSyncAssertF(get(), "Failed to open file \"%s\"", path);
+}
 
 }
--- Utils.h
+++ Utils.h
@@ -1,21 +1,32 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+
+
 namespace TdmSync {
 
 static const int SIZE_PATH = 4<<10;
 static const int SIZE_FILEBUFFER = 64<<10;
 static const int SIZE_LINEBUFFER = 16<<10;
 
-typedef std::unique_ptr<FILE, int (*)(FILE*)> stdioFileUniquePtr;
-class stdioFileHolder : public stdioFileUniquePtr {
-public:
-    stdioFileHolder(FILE *f) : stdioFileUniquePtr(f, fclose) {}
-    stdioFileHolder(const char *path, const char *mode) : stdioFileUniquePtr(fopen(path, mode), fclose) { TdmSyncAssertF(get(), "Failed to open file \"%s\"", path); }
-    operator FILE*() const { return get(); }
-};
 
 template<class T> void AppendVector(std::vector<T> &dst, const std::vector<T> &src) {
     dst.insert(dst.end(), src.begin(), src.end());
 }
+
+
+typedef std::unique_ptr<FILE, int (*)(FILE*)> StdioFileUniquePtr;
+/**
+ * RAII wrapper around FILE* from stdio.h.
+ * Automatically closes file on destruction.
+ */
+class StdioFileHolder : public StdioFileUniquePtr {
+public:
+    StdioFileHolder(FILE *f);
+    StdioFileHolder(const char *path, const char *mode);    //checks that file is opened successfully
+    ~StdioFileHolder();
+    operator FILE*() const { return get(); }
+};
 
 }
